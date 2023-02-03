6b8622f21436eaed1255a4602b8a0b0273d4f369
Ryan Holt <ryan.holt@akidolabs.com>
2023-02-03 12:51:06 -0600

    Expose CustomFilter.

--- custom_logging/__init__.py
+++ custom_logging/__init__.py
@@ -2,6 +2,6 @@
 
 """Initialize the package."""
 
-__all__ = ["configure_logging", "get_custom_logger", "get_log_file_from_handler"]
+__all__ = ["configure_logging", "get_custom_logger", "get_log_file_from_handler", "CustomFilter"]
 
-from .__main__ import configure_logging, get_custom_logger, get_log_file_from_handler
+from .__main__ import configure_logging, get_custom_logger, get_log_file_from_handler, CustomFilter
